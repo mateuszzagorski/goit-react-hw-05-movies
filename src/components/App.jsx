311d97b288ee7bea53968477d7fd0a4bf9ab1da5
AnnaIstochnikova <anya.istochnikova@gmail.com>
2023-11-09 09:51:45 +0100

    Create basic routes map and links

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+
+import { Header } from './Header';
+import { Home } from './Home';
+import { Movies } from './Movies';
+import { MovieDetails } from './MovieDetails';
+
 export const App = () => {
-  return <>Test</>;
+  return (
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="movies/:id" element={<MovieDetails />} />
+      </Route>
+    </Routes>
+  );
 };
