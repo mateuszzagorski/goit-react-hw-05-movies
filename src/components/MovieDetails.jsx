775110a0fb41b1d7945e3c03232db75f7da90b5c
AnnaIstochnikova <anya.istochnikova@gmail.com>
2023-11-10 07:25:22 +0100

    Fix movie details part

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -4,18 +4,36 @@ import { useEffect, useState } from 'react';
 import { getMovieDetails } from '../services/ApiRequests';
 
 export const MovieDetails = () => {
-  const [movieDetails, setMovieDetails] = useState([]);
+  const [movie, setMovie] = useState([]);
+  const [genres, setGenres] = useState([]);
   const { id } = useParams();
+
   useEffect(() => {
     getMovieDetails(id)
       .then(data => {
-        setMovieDetails(data);
         console.log(data);
+        setMovie(data);
+        setGenres(data.genres);
       })
       .catch(error => {
         console.error(error);
       });
   }, []);
-  console.log(movieDetails);
-  return <div>MovieDetails: {movieDetails.overview}</div>;
+
+  const votePercentage = (movie.vote_average * 10).toFixed(0) + '%';
+  return (
+    <div>
+      <h2>
+        {movie.title} ({movie.release_date})
+      </h2>
+      {movie.overview}
+      {movie.vote_average > 0 ? <p>User Score: {votePercentage}</p> : null}
+      <h3>Overview</h3>
+      <p>{movie.overview}</p>
+      <h3>Genres</h3>
+      <p>{genres.map(genre => genre.name).join(`, `)}</p>
+    </div>
+  );
 };
+
+// getGenres();
