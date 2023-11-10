8c333ced7f8661ea21f0e5a6f4a51925bef9dd41
AnnaIstochnikova <anya.istochnikova@gmail.com>
2023-11-10 07:24:27 +0100

    Update movie list

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,20 @@
+import { Link, useParams } from 'react-router-dom';
+
+export const MoviesList = ({ data }) => {
+  return (
+    <>
+      <ul>
+        {data.map(movie => {
+          console.log(movie);
+          return (
+            <li key={movie.id}>
+              <Link to={`/movies/${movie.id}`}>
+                <h3>{movie.title}</h3>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
